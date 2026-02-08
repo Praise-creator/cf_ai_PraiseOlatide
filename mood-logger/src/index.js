94b86548f41027f358151d63f82e59bbca4d06be
Praise <olatide.praise7@gmail.com>
2026-02-07 19:04:22 -0500

    Update index.js

--- mood-logger/src/index.js
+++ mood-logger/src/index.js
@@ -9,7 +9,35 @@
  */
 
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+    async fetch(request, env, ctx) {
+        
+        const url = new URL(request.url);
+        const path = url.pathname;
+
+        
+        if (path === "/api/entries") {
+            const results = await env.mood_logger_db.prepare("SELECT * FROM entries").all();
+            return new Response(JSON.stringify(results), {
+                headers: { "Content-Type": "application/json" },
+            });
+        }
+
+		if (path === "/api/add-entry" && request.method === "POST") {
+			const body = await request.json();
+			const raw_text = body.raw_text;
+			const input_type = body.input_type;
+
+			await env.mood_logger_db
+				.prepare("INSERT INTO entries (raw_text, input_type) VALUES (?, ?)")
+				.bind(raw_text, input_type)
+				.run();
+
+			return new Response(JSON.stringify({ success: true, message: "Entry added successfully" }), 
+			{headers: { "Content-Type": "application/json" }});
+		}
+
+        // Default response for any other route
+        return new Response("Mood Logger API");
+    },
+	
 };
